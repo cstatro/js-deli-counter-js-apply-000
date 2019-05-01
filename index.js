267ad8221173d3da67480aea1009e2304485ceec
Colin Tatro <cstatro@gmail.com>
2019-05-01 01:59:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ var katzDeliLine = []
 function takeANumber(deliLine,deliPatron){
   deliLine.push(deliPatron)
   return `Welcome, ${deliPatron}. You are number ${deliLine.length} in line.`
+}
 
+function nowServing (deliLine){
+  if (deliLine.length === 0){
+    return "the line is empty!"
+  }
+  else {
+    let currentPatron = deliLine.shift()
+    return `Now serving ${currentPatron}.`
+  }
 }
